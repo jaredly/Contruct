a9b60014c3943a6544674a2cb95877c3e1705099
ashleysbrain <ashleysbrain@8608462f-0e06-4f73-9daa-bc286878d562>
2009-12-28 21:05:16 +0000

    Versioned 0.99.72 for build.

--- IDE/StdAfx.h
+++ IDE/StdAfx.h
@@ -37,8 +37,8 @@
 // Construct Version Information
 // Current version:
 
-#define CONSTRUCT_VERSION_ID		9970
-#define CONSTRUCT_VERSION_STRING	"0.99.7"
+#define CONSTRUCT_VERSION_ID		9972
+#define CONSTRUCT_VERSION_STRING	"0.99.72"
 
 #define VC_EXTRALEAN				// Exclude rarely-used stuff from Windows headers
 #ifndef _CRT_SECURE_NO_DEPRECATE
